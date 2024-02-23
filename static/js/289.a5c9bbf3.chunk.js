87669861ed0ea316de20a63c6bfd8ba0ca3fc52d
trongnguyen74 <trongnguyenvn74@gmail.com>
2024-02-23 13:11:58 +0700

    Updates

--- static/js/289.a5c9bbf3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkjadecrochet=self.webpackChunkjadecrochet||[]).push([[289],{289:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=289.a5c9bbf3.chunk.js.map
---
+++ static/js/289.f43af44c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjadecrochet=self.webpackChunkjadecrochet||[]).push([[289],{3289:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.f43af44c.chunk.js.map
